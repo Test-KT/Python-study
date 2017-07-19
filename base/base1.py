9a57d703c2956aa23339618828466ec2f79ad8c6
lishoulin <408930131@qq.com>
2017-07-19 09:20:00 +0800

    commit

---
+++ base/base1.py
@@ -0,0 +1,17 @@
+
+def inputWe():
+    s=input('输入你的体重!\n')
+    weigth=int(s)
+
+    if(weigth<18):
+        print('过轻!')
+    elif weigth<25:
+        print('正常!')
+    elif weigth<28:
+        print('过重!')
+    elif weigth<32:
+        print('肥胖！')
+    else:
+        print('严重肥胖')
+
+inputWe()
